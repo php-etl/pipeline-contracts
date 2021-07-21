bedc19e9ddd4e4eac162c0bb4645a796efbd6513
Julien Marcon <julien@kiboko.fr>
2021-07-21 10:46:21 +0200

    Update FlushableInterface.php

--- src/FlushableInterface.php
+++ src/FlushableInterface.php
@@ -4,6 +4,9 @@ namespace Kiboko\Contract\Pipeline;
 
 use Kiboko\Contract\Bucket\ResultBucketInterface;
 
+/**
+ * @template T
+ */
 interface FlushableInterface
 {
     public function flush(): ResultBucketInterface;
