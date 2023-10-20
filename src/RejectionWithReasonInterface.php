37ac1909ac3de27c4f3721837b1beab41e26f1a7
GitHub Action <actions@github.com>
2023-10-20 13:47:02 +0000

    [rector] Rector fixes

--- src/RejectionWithReasonInterface.php
+++ src/RejectionWithReasonInterface.php
@@ -6,6 +6,5 @@ namespace Kiboko\Contract\Pipeline;
 
 interface RejectionWithReasonInterface extends RejectionInterface
 {
-    /** @param array|object $rejection */
     public function rejectWithReason(object|array $rejection, string $reason, null|\Throwable $exception = null): void;
 }
