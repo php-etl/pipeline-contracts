1fdcf91829b1f6c28f7737198c0f94c6e0b1a812
Grégory Planchat <gregory@kiboko.fr>
2023-11-13 14:06:17 +0100

    Fixed contracts on the pipeline rejection

--- src/StepRejectionInterface.php
+++ src/StepRejectionInterface.php
@@ -6,12 +6,8 @@ namespace Kiboko\Contract\Pipeline;
 
 interface StepRejectionInterface
 {
-    public function initialize(): void;
-
     /** @param non-empty-array<mixed>|object $rejection */
     public function reject(array|object $rejection, \Throwable $exception = null): void;
 
     public function rejectWithReason(array|object $rejection, string $reason, \Throwable $exception = null): void;
-
-    public function teardown(): void;
 }
