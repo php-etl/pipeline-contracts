d56df802ca88b42e4af94e188e47af7056182a54
Grégory Planchat <gregory@kiboko.fr>
2023-11-14 17:45:56 +0100

    Added the JobCode to the SchedulingInterface::job() method arguments

--- src/SchedulingInterface.php
+++ src/SchedulingInterface.php
@@ -8,5 +8,5 @@ use Kiboko\Contract\Satellite\RunnableInterface;
 
 interface SchedulingInterface
 {
-    public function job(JobCodeInterface $job, RunnableInterface $job): self;
+    public function job(JobCodeInterface $job, RunnableInterface $runnable): self;
 }
